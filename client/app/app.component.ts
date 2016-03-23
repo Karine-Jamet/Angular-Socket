8c704182b8e5c0b0b1393d4d5777860ea5e934f2
Guillaume BORG <guillaume.unice@gmail.com>
2016-03-23 14:00:31 +0100

    beginning implem http provider

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,30 +1,46 @@
 import {Component,OnInit} from 'angular2/core';
 import {SocketService} from './socket.service';
+import {HTTP_PROVIDERS}    from 'angular2/http';
+import {Http, Response} from 'angular2/http';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
-    selector: 'my-app',
+  selector: 'my-app',
     template:`
-  <h1>Truc</h1>
-<input type="text" [(ngModel)]="check" name="name" placeholder="your name"/>
-<input type="submit" (click)="sendToCheck(check)" />`,
-providers: [SocketService]
+      <h1>Truc</h1>
+    <input type="text" [(ngModel)]="check" name="name" placeholder="your name"/>
+    <input type="submit" (click)="sendToCheck(check)" />
+    `,
+    providers: [HTTP_PROVIDERS, SocketService]
 
 })
 
 
 export class AppComponent implements OnInit {
-   constructor(private _socketService: SocketService){}
+  constructor(private _socketService: SocketService, private http: Http){}
   // constructor(private _heroService: HeroService){}
 
   check: string;
 
   sendToCheck = function(e){
-
-      this._socketService.sock.send(JSON.stringify({msg : e}));
     //envoyer en socket
+    this._socketService.sock.send(JSON.stringify({msg : e}));
   }
   ngOnInit() {
     this._socketService.connect();
+    
     console.log('ngOnInit');
+    this.getTest();
   }
+
+ getTest() {
+   this.http.get('http://127.0.0.1:9998/api/')
+     .map((res: Response) => res.json())
+      .subscribe(
+      data => { console.log(data); },
+        err => console.error(err),
+        () => console.log('done')
+      );
+  }
+
 }
